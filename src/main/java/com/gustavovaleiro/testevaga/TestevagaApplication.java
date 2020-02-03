dbeb7ff7881b9644ef7fc413205b590e03d5a4a1
Gustavo Valeiro <gustavovaleiro.cc@gmail.com>
2020-02-03 15:00:31 -0300

    first commit

---
+++ src/main/java/com/gustavovaleiro/testevaga/TestevagaApplication.java
@@ -0,0 +1,13 @@
+package com.gustavovaleiro.testevaga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestevagaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestevagaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gustavovaleiro/testevaga/TestevagaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustavovaleiro.testevaga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestevagaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
